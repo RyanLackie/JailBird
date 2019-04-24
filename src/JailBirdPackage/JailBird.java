c3310e0ed14683949fa58cd05a784dcce0f80e99
Ryan Lackie <ryanwlackie@gmail.com>
2019-04-24 12:42:20 -0400

    4/24 Update
    Initial JailBird file move onto github.

---
+++ src/JailBirdPackage/JailBird.java
@@ -0,0 +1,216 @@
+package JailBirdPackage;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.*;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef.HWND;
+import com.sun.jna.win32.W32APIOptions;
+
+public class JailBird {
+
+	static JFrame frame = new JFrame("JailBird");
+	static JTextArea Console;
+	static JButton Start;
+	static JButton Stop;
+	static JLabel label;
+	
+	static boolean running = false;
+	static boolean flag = false;
+	
+	static int secDelay = 5;
+	
+	static String[] log = {"", "", "", "", "", "", ""};
+	static int index = 0;
+	
+	public static void main(String[] args) throws InterruptedException {
+		init();	
+		//Loop
+		while (true) {
+			//if running
+			if (running) {
+				if (!flag)
+					target();
+				
+				if (flag) {
+					label.setForeground(Color.RED);
+					label.setText("Running");
+					
+					User32 user32 = User32.instance;  
+		            HWND hWnd = user32.FindWindow("ApplicationFrameWindow", "Sea of Thieves"); //ClassName, WindowName
+		            if (hWnd == null) {
+		            	running = false;
+		            	flag = false;
+		            }
+		            user32.ShowWindow(hWnd, User32.SW_SHOW);  
+		            user32.SetForegroundWindow(hWnd);
+		            
+					
+			    	Random rand = new Random();
+			    	int r = rand.nextInt(12)+1;
+			    	
+			    	switch (r) {
+			    		case 1:	robKeyHold('A');
+			    				break;
+			    		case 2:	robKeyHold('W');
+			    				break;
+			    		case 3:	robKeyHold('S');
+			    				break;
+			    		case 4:	robKeyHold('D');
+			    				break;
+			    		case 5:	robKeyPress('6');
+			    				break;
+			    		case 6:	robKeyPress('7');
+			    				break;
+			    		case 7:robKeyPress('8');
+								break;
+			    		case 8:robKeyPress('9');
+								break;
+			    		case 9:robKeyPress('0');
+								break;
+			    		case 10:robKeyPress('l');
+								break;
+			    		case 11:robKeyPress('b');
+								break;
+			    		case 12:robKeyPress('i');
+								break;
+			    	}
+			    	Thread.sleep(secDelay * 1000);
+				}
+			}
+			//if not running
+			if (!running) {
+				label.setForeground(Color.BLACK);
+				label.setText("Standby");
+			}
+		}
+		
+	}
+	
+	public static void target() {
+		logBuilder("Searching for Sea of Thieves");
+		
+		HWND find = User32.instance.FindWindow
+				("ApplicationFrameWindow", "Sea of Thieves");
+		if (find == null) {
+			flag = false;
+			logBuilder("Can not find Sea of Thieves");
+			running = false;
+		}
+		else {
+			flag = true;
+			logBuilder("Connected to Sea of Thieves");
+		}
+	}
+	
+	public static void init() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(348, 198);
+		frame.getContentPane().setLayout(null);
+		
+		Start = new JButton("Start");
+		Start.setBackground(Color.GREEN);
+		Start.setBounds(10, 11, 62, 55);
+		frame.getContentPane().add(Start);
+		
+		Stop = new JButton("Stop");
+		Stop.setBackground(Color.RED);
+		Stop.setBounds(10, 77, 62, 55);
+		frame.getContentPane().add(Stop);
+		
+		Start st = new Start();
+		Start.addActionListener(st);
+		Stop sp = new Stop();
+		Stop.addActionListener(sp);
+		
+		Console = new JTextArea();
+		Console.setEditable(false);
+		Console.setBounds(112, 11, 210, 121);
+		frame.getContentPane().add(Console);
+		
+		
+		label = new JLabel();
+		label.setText("Standby");
+		label.setBounds(112, 139, 46, 14);
+		frame.getContentPane().add(label);
+		
+		frame.setVisible(true);
+	}
+	
+	public static class Start implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			running = true;
+		}
+	}
+	public static class Stop implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			running = false;
+		}
+	}
+	
+	public interface User32 extends W32APIOptions {
+        User32 instance = (User32) Native.loadLibrary("user32", User32.class, DEFAULT_OPTIONS);
+        boolean ShowWindow(HWND hWnd, int nCmdShow);
+        boolean SetForegroundWindow(HWND hWnd);
+        HWND FindWindow(String winClass, String title);
+        int SW_SHOW = 1;
+    }
+	
+	public static void logBuilder(String st) {
+		if (index < 7)
+			log[index] = st;
+		else {
+			for (int i = 0; i < 6; i++) {
+				log[i] = log[i+1];
+			}
+			log[6] = st;
+		}
+		
+		Console.setText(log[0]+"\n"+log[1]+"\n"+log[2]+"\n"+log[3]+"\n"+log[4]+"\n"+log[5]+"\n"+log[6]);
+		
+		if (index < 8)
+			index++;
+	}
+	
+	public static void robKeyPress(char c) throws InterruptedException {
+		logBuilder(c + " is pressed");
+		//Console.append("\n" + c + " is pressed");
+		
+		try {
+			Robot robot = new Robot();
+			robot.keyPress(c);
+	    	robot.keyRelease(c);
+		} 
+		catch (AWTException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public static void robKeyHold(char c) throws InterruptedException {
+    	logBuilder(c + " is being held for 1 second");
+    	//Console.append("\n" + c + " is being held for 1 second");
+    	
+    	long i = System.currentTimeMillis();
+    	while (true) {
+	    	try {
+				Robot robot = new Robot();
+				robot.keyPress(c);
+				if (System.currentTimeMillis() >= i+1000) {
+					robot.keyRelease(c);
+					logBuilder(c + " is released");
+					//Console.append("\n" + c + " is released");
+					break;
+				}
+			} 
+	    	catch (AWTException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+}
